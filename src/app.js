5fa5d2a972e28b285e9d169d71705e87b0becf63
RahulSeelam0 <seelamrahulkandukur@gmail.com>
2025-05-21 18:44:13 +0530

    Added routes and controllers for user

--- src/app.js
+++ src/app.js
@@ -9,14 +9,20 @@ app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials : true,
 }));
-app.get('/', (req, res) => {
-    res.send("Hello, Everything is working fine");
-});
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+import userRouter from './routes/user.routes.js';
 
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+app.get('/', (req, res) => {
+    res.send("Hello, Everything is working fine");
+});
 
 export { app }
